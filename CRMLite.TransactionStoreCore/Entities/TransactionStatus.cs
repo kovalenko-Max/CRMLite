0d71add2861f5bafe1b4ce7b6bb258e82e9e0a67
Pavlo Iushkovskyi <dneprpi@gmail.com>
2021-08-02 17:17:28 +0300

    added models
    Transaction and Wallet

---
+++ CRMLite.TransactionStoreCore/Entities/TransactionStatus.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CRMLite.TransactionStoreDomain.Entities
+{
+    public class TransactionStatus
+    {
+        public bool IsSuccessfull { get; set; }
+    }
+}
