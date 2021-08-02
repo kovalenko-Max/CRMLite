2d6cbc6a3cb219f4b228c04aa5e451ea45ef106b
Maks Kovalenko <max.don@live.ru>
2021-08-02 19:35:36 +0300

    Create TransactionStoreDB

---
+++ CRMLite.TransactionStoreDatabase/CRMLite/Tables/Wallet.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [CRMLite].[Wallet] (
+    [ID]       UNIQUEIDENTIFIER NOT NULL,
+    [Currency] TINYINT       NOT NULL,
+    [Amount]   DECIMAL (18)  NOT NULL,
+    CONSTRAINT [PK_WALLET] PRIMARY KEY CLUSTERED ([ID] ASC),
+    CONSTRAINT [Wallet_fk0] FOREIGN KEY ([Currency]) REFERENCES [CRMLite].[Currency] ([ID])
+);
+
