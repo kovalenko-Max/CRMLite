2018f3b6f2743d01baacfc04c722ec3de584e9cc
Maxim <aryewar@gmail.com>
2021-08-03 09:27:06 +0300

    Add Project and add CRUD

---
+++ CRMLite.CRMDB/dbo/Stored Procedures/UpdateLead.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [CRMLite].[UpdateLead] @Id UNIQUEIDENTIFIER
+	,@Lastname NVARCHAR(255)
+	,@Firstname NVARCHAR(255)
+	,@PasportNumber NVARCHAR(8)
+	,@TIN NVARCHAR(12)
+	,@Email NVARCHAR(255)
+	,@Password NVARCHAR(255)
+	,@Role NVARCHAR(255)
+AS
+UPDATE [CRMLite].[Leads]
+SET [Id] = @Id
+	,[Lastname] = @Lastname
+	,[Firstname] = @Firstname
+	,[PasportNumber] = @PasportNumber
+	,[TIN] = @TIN
+	,[Email] = @Email
+	,[Password] = @Password
+	,[Role] = @Role
+WHERE [Id] = @Id
---
+++ CRMLite.CRMDB/dbo/Tables/Leads.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [CRMLite].[Leads] (
+    [Id]            UNIQUEIDENTIFIER NOT NULL,
+    [Lastname]      NVARCHAR (255)   NOT NULL,
+    [Firstname]     NVARCHAR (255)   NOT NULL,
+    [PasportNumber] NVARCHAR (8)     NOT NULL,
+    [TIN]           NVARCHAR (12)    NOT NULL,
+    [Email]         NVARCHAR (255)   NOT NULL,
+    [Password]      NVARCHAR (255)   NOT NULL,
+    [Role]          NVARCHAR (255)   NOT NULL,
+    CONSTRAINT [PK_LEADS] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
