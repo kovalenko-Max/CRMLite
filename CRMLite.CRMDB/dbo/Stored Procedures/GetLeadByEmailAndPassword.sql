79fa116c710118ebbd9280b17589bdb5c716b6db
Maxim <aryewar@gmail.com>
2021-08-03 09:27:06 +0300

    Add Project and add CRUD

---
+++ CRMLite.CRMDB/dbo/Stored Procedures/GetLeadByEmailAndPassword.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [CRMLite].[GetLeadByEmailAndPassword] @Email NVARCHAR(255)
+	,@Password NVARCHAR(255)
+AS
+SELECT *
+FROM [CRMLite].[Leads]
+WHERE [Email] = @Email
+	AND [Password] = @Password
