09d108a59e8f1d51d100a1f6bbb4060f5ce3da71
Alexandr Shpiganovich <semben22@gmail.com>
2021-08-04 16:13:29 +0300

    Add CurrencyRepository

---
+++ CRMLite.TransactionStoreCore/Interfaces/ICurrencyRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CRMLite.TransactionStoreDomain.Interfaces
+{
+    public interface ICurrencyRepository : IRepository
+    {
+        Task<List<string>> GetAllCurrencyAsync();
+    }
+}
