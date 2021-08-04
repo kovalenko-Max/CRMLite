9e7664cc7cf793b42676c101b0b18ef18c25d4f7
Konstantin <murashko.konst.andr@gmail.com>
2021-08-04 13:21:28 +0300

    Add TransactionRepository

---
+++ CRMLite.TransactionStoreCore/Interfaces/ITransactionRepository.cs
@@ -0,0 +1,14 @@
+﻿using CRMLite.TransactionStoreDomain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CRMLite.TransactionStoreDomain.Interfaces
+{
+    public interface ITransactionRepository : IRepository
+    {
+        Task<IEnumerable<Transaction>> GetAllTransactionByLeadIDAsync(Guid leadID);
+        Task<IEnumerable<Transaction>> GetAllTransactionByWalletIDAsync(Guid walletID);
+        Task CreateTransactionAsync(Transaction transaction);
+    }
+}
