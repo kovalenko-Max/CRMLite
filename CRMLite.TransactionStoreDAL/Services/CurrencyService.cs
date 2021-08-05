b3885dbc5fde6d3b461148891226b345a4f62e5d
Rostislav <skochmen@gmail.com>
2021-08-05 11:57:32 +0300

    Add CurrencyService

---
+++ CRMLite.TransactionStoreDAL/Services/CurrencyService.cs
@@ -0,0 +1,24 @@
+﻿using CRMLite.TransactionStoreDomain.Interfaces.IRepositories;
+using CRMLite.TransactionStoreDomain.Interfaces.IServices;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CRMLite.TransactionStoreBLL.Services
+{
+    public class CurrencyService : ICurrencyService
+    {
+        private ICurrencyRepository _repository;
+
+        public CurrencyService(ICurrencyRepository currencyRepository)
+        {
+            _repository = currencyRepository;
+        }
+
+        public async Task<List<string>> GetAllCurrencyAsync()
+        {
+            var responce = await _repository.GetAllCurrencyAsync();
+
+            return responce;
+        }
+    }
+}
