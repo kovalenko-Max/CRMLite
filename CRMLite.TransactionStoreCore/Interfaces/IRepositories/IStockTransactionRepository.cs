4c5df3503888c42a4a1f1f66238fed5a2d76437e
Maks Kovalenko (Lenovo) <max.don@live.ru>
2021-08-05 12:19:15 +0300

    Create StockTransactionController and Service

---
+++ CRMLite.TransactionStoreCore/Interfaces/IRepositories/IStockTransactionRepository.cs
@@ -0,0 +1,14 @@
+﻿using CRMLite.TransactionStoreDomain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CRMLite.TransactionStoreDomain.Interfaces.IRepositories
+{
+    public interface IStockTransactionRepository
+    {
+        Task CreateStockTransactionAsync(StockTransaction stockTransaction);
+        Task<IEnumerable<StockTransaction>> GetAllStockTransactionByStockID(Guid stockID);
+        Task<IEnumerable<StockTransaction>> GetAllTransactionByLeadIDAsync(Guid leadID);
+    }
+}
