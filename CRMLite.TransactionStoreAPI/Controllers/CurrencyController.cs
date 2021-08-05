67d47402114f2084a2bb2aa79e374b78df56d180
Alexey <nhevgevgev@gmail.com>
2021-08-05 13:46:06 +0300

    Add OperationType Service

--- CRMLite.TransactionStoreAPI/Controllers/CurrencyController.cs
+++ CRMLite.TransactionStoreAPI/Controllers/CurrencyController.cs
@@ -1,8 +1,6 @@
 ﻿using CRMLite.TransactionStoreDomain.Interfaces.IServices;
 using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace CRMLite.TransactionStoreAPI.Controllers
