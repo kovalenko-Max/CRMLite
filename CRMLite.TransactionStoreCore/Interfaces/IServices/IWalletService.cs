7d66dd0d599ebac5aef43580e052017a747984fe
Alexey <nhevgevgev@gmail.com>
2021-08-05 11:29:59 +0300

    Add Wallet Service
    Add Wallet Service

---
+++ CRMLite.TransactionStoreCore/Interfaces/IServices/IWalletService.cs
@@ -0,0 +1,15 @@
+﻿using CRMLite.TransactionStoreDomain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRMLite.TransactionStoreDomain.Interfaces.IServices
+{
+    public interface IWalletService
+    {
+        Task CreateWalletWithinLeadAsync(Wallet wallet);
+        Task<IEnumerable<Wallet>> GetAllWalletsByLeadIDAsync(Guid leadID);
+        Task<Wallet> GetWalletByIDAsync(Guid id);
+    }
+}
