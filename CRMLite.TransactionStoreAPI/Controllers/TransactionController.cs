edbad718a598348c6cb259fe4f717c3b2d358248
Pavlo <dneprpi@gmail.com>
2021-08-05 13:12:30 +0300

    Created Balance Controller
    edited wallet and Transaction controllers

--- CRMLite.TransactionStoreAPI/Controllers/TransactionController.cs
+++ CRMLite.TransactionStoreAPI/Controllers/TransactionController.cs
@@ -7,8 +7,8 @@ using System.Threading.Tasks;
 
 namespace CRMLite.TransactionStoreAPI.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
+    [Route("api/[controller]")]
     public class TransactionController : Controller
     {
         ITransactionService _transactionService;
