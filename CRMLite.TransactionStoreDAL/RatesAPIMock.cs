c82e60c126156844d61d65245074e863290719da
Pavlo <dneprpi@gmail.com>
2021-08-05 11:11:49 +0300

    Created Balance Service
    plus BalanceCounter and RatesAPIMock

---
+++ CRMLite.TransactionStoreDAL/RatesAPIMock.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CRMLite.TransactionStoreBLL
+{
+    public class RatesAPIMock
+    {
+        private static Dictionary<string, decimal> currencies = new Dictionary<string, decimal>
+        {
+            ["MUSD"] = 1,
+            ["MUAH"] = 0.037M,
+            ["MRUB"] = 0.014M,
+            ["MGBP"] = 1.39M,
+            ["MEUR"] = 1.18M
+        };
+
+        public Dictionary<string, decimal> GetAllRates()
+        {
+            return currencies;
+        }
+
+        public decimal GetRateForCurrency (string currencyTitle)
+        {
+            if (currencies.ContainsKey(currencyTitle))
+            {
+                return currencies[currencyTitle];
+            }
+            
+            throw new ArgumentException("Wrong currency Title"); 
+        }
+    }
+}
