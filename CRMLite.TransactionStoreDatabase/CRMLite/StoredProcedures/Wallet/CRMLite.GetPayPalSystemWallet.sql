7281f65bb2a726da6ea3132ffd3694ed3db38e2e
Maks Kovalenko (Lenovo) <max.don@live.ru>
2021-08-25 11:49:14 +0300

    Create PayPal system wallet

---
+++ CRMLite.TransactionStoreDatabase/CRMLite/StoredProcedures/Wallet/CRMLite.GetPayPalSystemWallet.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [CRMLite].[GetPayPalSystemWallet]
+AS
+	SELECT 
+	ID, 
+	CurrencyID,
+	Amount
+	from [CRMLite].[Wallets] W
+	join [CRMLite].[Balance] B on W.ID = B.WalletID
+
+	where B.LeadID = '471A2146-82BC-4C18-9425-2D8CC89F8540'
--- CRMLite.TransactionStoreDatabase/CRMLite/Tables/Balance.sql
+++ CRMLite.TransactionStoreDatabase/CRMLite/Tables/Balance.sql
@@ -4,3 +4,7 @@
     CONSTRAINT [Balance_fk0] FOREIGN KEY ([WalletID]) REFERENCES [CRMLite].[Wallets] ([ID])
 );
 GO
+
+CREATE INDEX [IX_Balance_LeadID] ON [CRMLite].[Balance] ([LeadID])
+GO
+CREATE INDEX [IX_Balance_WalletID] ON [CRMLite].[Balance] ([WalletID])
